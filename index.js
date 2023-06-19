e618fe955e4c6553f0c9f809c4c0a8d53dc36a8c
ilham puji santoso <ilhammahel16@gmail.com>
2023-06-19 16:37:01 +0700

    add cert

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')
-const app = express()
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
+const path = require('path')
+
+const app = express();
 const apiPort = 5000
 
 const Routes = require('./routes');
+var key = fs.readFileSync(path.join(__dirname, 'cert', 'key.pem'));
+var cert = fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'));
+var options = {
+  key: key,
+  cert: cert
+};
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -20,4 +30,7 @@ app.get('/', (req, res) => {
 
 app.use('/api', Routes)
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+var server = https.createServer(options, app);
+server.listen(apiPort, () => {
+  console.log("server starting on port : " + apiPort)
+});
