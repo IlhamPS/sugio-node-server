3ef0c55e4b2405230d4c9926fc780d1f8447e325
ilham puji santoso <ilhammahel16@gmail.com>
2023-08-04 23:21:19 +0700

    remove google drive

--- index.js
+++ index.js
@@ -3,34 +3,37 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 const https = require('https');
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+const url = require('url');
 
 const app = express();
 const apiPort = 5000
 
 const Routes = require('./routes');
-var key = fs.readFileSync(path.join(__dirname, 'cert', 'key.key'));
-var cert = fs.readFileSync(path.join(__dirname, 'cert', 'cert.key'));
-var options = {
-  key: key,
-  cert: cert
-};
+// var key = fs.readFileSync(path.join(__dirname, 'cert', 'key.key'));
+// var cert = fs.readFileSync(path.join(__dirname, 'cert', 'cert.key'));
+// var options = {
+//   key: key,
+//   cert: cert
+// };
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  console.log('run')
+  const pathFile = path.join(__dirname, 'api/file/files/file_1691162681443_LOGOSMK.png' )
+  const testUrl = url.pathToFileURL(pathFile)
   res.send({
     name: 'test',
-    msg: 'Halo'
+    msg: 'Halo',
+    file: testUrl
   })
 })
 
 app.use('/api', Routes)
 
-var server = https.createServer(options, app);
-server.listen(apiPort, () => {
+// var server = https.createServer(options, app);
+app.listen(apiPort, () => {
   console.log("server starting on port : " + apiPort)
 });
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,38 @@
 const express = require('express');
+const path = require('path');
+const url = require('url')
+var fs = require('fs');
 
-const { multer, authenticateGoogle, uploadToGoogleDrive, deleteFile } = require('../api/file/upload');
+const { 
+  multer, 
+  // authenticateGoogle, 
+  // uploadToGoogleDrive, 
+  // deleteFile 
+} = require('../api/file/upload');
 
 const router = express.Router();
 
 router.post("/upload", multer.single("file"), async (req, res, next) => {
   try {
-    console.log('upload!');
     if (!req.file) {
-      res.status(400).send("No file uploaded.");
+      // res.status(400).send("No file uploaded.");
+      res.send({
+        status: false,
+        msg: 'File Must Not Empty'
+      })
       return;
     }
-    const auth = authenticateGoogle();
-    const response = await uploadToGoogleDrive(req.file, auth);
-    deleteFile(req.file.path);
-    res.status(200).json({ response });
+    const pathFile = path.join(__dirname, `../api/file/files/${req.file.filename}` )
+    const testUrl = url.pathToFileURL(pathFile);
+    res.send({
+      status: true,
+      msg: 'Upload Success',
+      url: testUrl
+    })
+    // const auth = authenticateGoogle();
+    // const response = await uploadToGoogleDrive(req.file, auth);
+    // deleteFile(req.file.path);
+    // res.status(200).json({ status: true, msg: 'Upload Success' });
   } catch (err) {
     console.log(err);
     res.status(400).json({ err });
